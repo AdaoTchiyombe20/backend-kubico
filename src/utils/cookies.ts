44e1d16278187a039aee55eaab184fd1a2cda8b7
Adão Tchiyombe20 <adaotchiyombe@gmail.com>
2026-05-21 00:01:24 +0100

    Fix some problem with cookies

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -16,7 +16,7 @@ export function setCookie(
   const defaultOptions: CookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'lax',
     maxAge: 7 * 24 * 60 * 60 * 1000, 
   };
 
