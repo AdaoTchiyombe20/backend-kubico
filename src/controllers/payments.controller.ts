5f6f3284889ddcfbc01174bfe77b55f5d971f0a1
Adão Tchiyombe20 <adaotchiyombe@gmail.com>
2026-05-26 00:33:50 +0100

    adding removing client_id in payments

--- src/controllers/payments.controller.ts
+++ src/controllers/payments.controller.ts
@@ -1,13 +1,17 @@
 import type {Request, Response,NextFunction} from "express";
 import { paymentDto, type PaymentDto } from "../dto/payment.dto.js";
 import { paymentsService } from "../services/payments.services.js";
-import { success } from "zod";
+import { AppError } from "../errors/App.Errors.js";
 export const paymentsController = {
     propertyPayment: async (req: Request, res: Response, next: NextFunction) => {
         try {
+            const client_id = req.accessUser?.profileId as string;
+            if(!client_id)
+                throw new AppError("id de client nao existente", 404)
+
             const paymentData : PaymentDto = paymentDto.parse(req.body);
 
-            const result = await paymentsService.processPayment(paymentData);
+            const result = await paymentsService.processPayment(Number(client_id),paymentData);
 
             res.json({
                 success: true,
--- src/dto/payment.dto.ts
+++ src/dto/payment.dto.ts
@@ -3,7 +3,6 @@ import z from 'zod';
 export const paymentDto = z.object({
    listed_property_id: z.number("Property ID must be a positive number").positive(),
    paymentType: z.enum(["DIRECT_PURCHASE", "NEGOCIATED_PURCHASE"], "Invalid payment type"),
-   client_id: z.number("Client ID must be a positive number").positive(),
 });
 
 export type PaymentDto = z.infer<typeof paymentDto>;
